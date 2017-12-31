6c45b2ac825598dc3a12b7dae0022df19c01da7d
浅浅浅 <mjyi93@qq.com>
2017-12-31 20:38:52 +0800

    add autolayout demo

---
+++ Demo-AutoLayout/Demo-AutoLayout/StackViewController.swift
@@ -0,0 +1,90 @@
+//
+//  StackViewController.swift
+//  Demo-AutoLayout
+//
+//  Created by mervin on 2017/12/31.
+//  Copyright © 2017年 mervin. All rights reserved.
+//
+
+import UIKit
+
+class StackViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addEntry(_ sender: Any) {
+        
+        guard let stack = stackView else { return }
+        let index = stack.arrangedSubviews.count - 1
+        let addView = stack.arrangedSubviews[index]
+        
+        guard let scroll = scrollView else { return }
+        
+        let offset = CGPoint(x: scroll.contentOffset.x,
+                             y: scroll.contentOffset.y + addView.frame.size.height)
+        
+        let newView = createEntry()
+        newView.isHidden = true
+        stack.insertArrangedSubview(newView, at: index)
+        
+        UIView.animate(withDuration: 0.25) {
+            newView.isHidden = false
+            scroll.contentOffset = offset
+        }
+    }
+    
+    @objc func deleteStackView(sender: UIButton) {
+        if let view = sender.superview {
+            UIView.animate(withDuration: 0.25, animations: {
+                view.isHidden = true
+            }, completion: { (success) in
+                view.removeFromSuperview()
+            })
+        }
+    }
+    
+    private func createEntry() -> UIView {
+        let date = DateFormatter.localizedString(from: Date(), dateStyle: .short, timeStyle: .none)
+        let number = "\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())"
+        
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .firstBaseline
+        stack.distribution = .fill
+        stack.spacing = 8
+        
+        let dateLabel = UILabel()
+        dateLabel.text = date
+        dateLabel.font = UIFont.preferredFont(forTextStyle: .body)
+
+        let numberLabel = UILabel()
+        numberLabel.text = number
+        numberLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+
+        let deleteButton = UIButton(type: .roundedRect)
+        deleteButton.setTitle("Delete", for: .normal)
+        deleteButton.addTarget(self, action: #selector(deleteStackView(sender:)), for: .touchUpInside)
+
+        stack.addArrangedSubview(dateLabel)
+        stack.addArrangedSubview(numberLabel)
+        stack.addArrangedSubview(deleteButton)
+        
+        return stack
+        
+    }
+
+    private func randomHexQuad() -> String {
+        return NSString(format: "%X%X%X%X",
+                        arc4random() % 16,
+                        arc4random() % 16,
+                        arc4random() % 16,
+                        arc4random() % 16
+            ) as String
+    }
+}
+
